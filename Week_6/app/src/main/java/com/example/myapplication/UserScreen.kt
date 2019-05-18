95ba27235de1bfcdc842d203d4135a26ecbb829b
NguyenHoaiNhi <nguyenhoainhi21101997@gmail.com>
2019-05-18 08:29:15 +0700

    first commit

---
+++ Week_6/app/src/main/java/com/example/myapplication/UserScreen.kt
@@ -0,0 +1,76 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.arch.persistence.room.Room
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import kotlinx.android.synthetic.main.user_screen.*
+import android.widget.ImageView
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.example.myapplication.R
+import com.example.myapplication.room.AppDatabase
+import com.example.myapplication.room.Task
+import com.example.myapplication.room.User
+import com.example.myapplication.room.UserDAO
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.user_screen.*
+
+
+class UserScreen : AppCompatActivity() {
+    var Users: ArrayList<User> = ArrayList()
+    lateinit var userAdapter: UserAdapter
+    lateinit var daoUser: UserDAO
+
+    var user = User()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_screen)
+        initRoomDatabase()
+        setupRecyclerView()
+        getUser()
+        btnAdd.setOnClickListener{
+            // db1 = AppDatabase.invoke(this)
+            user.name = edtUserID.text.toString()
+            daoUser.insert(user)
+            userAdapter.appenData(user)
+
+        }
+    }
+    private fun initRoomDatabase(){
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            DATABASE_NAME).allowMainThreadQueries()
+            .build()
+        daoUser = db.UserDAO()
+    }
+    private fun setupRecyclerView(){
+        rvUser.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
+        userAdapter = UserAdapter(Users, this)
+        rvUser.adapter = userAdapter
+        userAdapter.setListener(btRemove)
+        userAdapter.notifyDataSetChanged() // notify data changed
+
+    }
+    private val btRemove = object:ItemClickListener{
+        override fun btRemoveClicked(position: Int) {
+            Log.e("Users[position]", Users[position].toString())
+            daoUser.delete(Users[position])
+
+            userAdapter.removeItem(Users[position], position)
+        }
+
+    }
+    private fun getUser(){
+        val users = daoUser.getAll() // get Tasks from ROOM database
+        Log.i("User: ", users.toString())
+        this.Users.addAll(users) // add to task list
+        userAdapter.notifyDataSetChanged() // notify data changed
+    }
+
+
+}
